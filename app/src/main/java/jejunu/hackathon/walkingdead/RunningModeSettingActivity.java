643cb35d0bb7404513e11b34a8e95a1f8abc59db
Wef2 <qhrud5725@hanmail.net>
2016-07-29 19:19:27 +0900

    Google Map API -> Naver Map API

--- app/src/main/java/jejunu/hackathon/walkingdead/RunningModeSettingActivity.java
+++ app/src/main/java/jejunu/hackathon/walkingdead/RunningModeSettingActivity.java
@@ -4,26 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
+import com.nhn.android.maps.nmapmodel.NMapError;
 
-public class RunningModeSettingActivity extends AppCompatActivity implements GoogleMap.OnMarkerDragListener, OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class RunningModeSettingActivity extends NMapActivity{
 
-    private SupportMapFragment mapFragment;
+    private String CLIENT_ID = "gGTDc3aIdtkmsYRyE3u8";
+
+    private NMapView mMapView;
+    private NMapController mMapController;
     private Toolbar toolbar;
-    private GoogleMap mMap;
-    private LatLng startLatLng, endLatLng;
-    private boolean markerStatus = true;
-    private MarkerOptions startMarker, endMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +31,93 @@ public class RunningModeSettingActivity extends AppCompatActivity implements Goo
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("출발지 및 목적지 설정");
-        setSupportActionBar(toolbar);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        setSupportActionBar(toolbar);
+        mMapView = (NMapView)findViewById(R.id.mapView);
 
+        // set a registered Client Id for Open MapViewer Library
+        mMapView.setClientId(CLIENT_ID);
+
+        // initialize map view
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+
+        // register listener for map state changes
+        mMapView.setOnMapStateChangeListener(onMapViewStateChangeListener);
+        mMapView.setOnMapViewTouchEventListener(onMapViewTouchEventListener);
+
+        // use map controller to zoom in/out, pan and set map center, zoom level etc.
+        mMapController = mMapView.getMapController();
     }
+
+
+    /* MapView State Change Listener*/
+    private final NMapView.OnMapStateChangeListener onMapViewStateChangeListener = new NMapView.OnMapStateChangeListener() {
+
+        @Override
+        public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
+            if (errorInfo == null) { // success
+                mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 11);
+            } else { // fail
+            }
+        }
+
+        @Override
+        public void onAnimationStateChange(NMapView mapView, int animType, int animState) {
+        }
+
+        @Override
+        public void onMapCenterChange(NMapView mapView, NGeoPoint center) {
+        }
+
+        @Override
+        public void onZoomLevelChange(NMapView mapView, int level) {
+        }
+
+        @Override
+        public void onMapCenterChangeFine(NMapView mapView) {
+
+        }
+    };
+
+    private final NMapView.OnMapViewTouchEventListener onMapViewTouchEventListener = new NMapView.OnMapViewTouchEventListener() {
+
+        @Override
+        public void onLongPress(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onLongPressCanceled(NMapView mapView) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSingleTapUp(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onTouchDown(NMapView mapView, MotionEvent ev) {
+
+        }
+
+        @Override
+        public void onScroll(NMapView mapView, MotionEvent e1, MotionEvent e2) {
+        }
+
+        @Override
+        public void onTouchUp(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+
+        }
+
+    };
 
 
     @Override
@@ -52,8 +131,6 @@ public class RunningModeSettingActivity extends AppCompatActivity implements Goo
         switch (item.getItemId()) {
             case R.id.action_done:
                 Intent intent = new Intent(RunningModeSettingActivity.this, RunningActivity.class);
-                intent.putExtra("start", startLatLng);
-                intent.putExtra("end", endLatLng);
                 startActivity(intent);
                 super.onBackPressed();
                 return true;
@@ -61,57 +138,4 @@ public class RunningModeSettingActivity extends AppCompatActivity implements Goo
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng jeju = new LatLng(33.499234, 126.530714);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jeju, 13));
-        mMap.setOnMapClickListener(this);
-    }
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-        mMap.clear();
-        if(markerStatus){
-            startLatLng = latLng;
-        }
-        else{
-            endLatLng = latLng;
-        }
-        markerStatus = !markerStatus;
-
-        if(startLatLng != null){
-            startMarker = new MarkerOptions()
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.start))
-                    .position(startLatLng).draggable(true);
-            mMap.addMarker(startMarker);
-        }
-        if(endLatLng != null){
-            endMarker =  new MarkerOptions()
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.end))
-                    .position(endLatLng).draggable(true);
-            mMap.addMarker(endMarker);
-        }
-    }
-
-
-    @Override
-    public void onMarkerDragStart(Marker marker) {
-
-    }
-
-    @Override
-    public void onMarkerDrag(Marker marker) {
-
-    }
-
-    @Override
-    public void onMarkerDragEnd(Marker marker) {
-        if(marker.equals(startMarker)){
-            startLatLng = marker.getPosition();
-        }else if(marker.equals(endMarker)){
-            endLatLng = marker.getPosition();
-        }
-
-    }
 }
--- app/src/main/java/jejunu/hackathon/walkingdead/Zombie.java
+++ app/src/main/java/jejunu/hackathon/walkingdead/Zombie.java
@@ -1,19 +1,8 @@
 package jejunu.hackathon.walkingdead;
 
-import com.google.android.gms.maps.model.LatLng;
-
 /**
  * Created by replay on 2016. 7. 12..
  */
 public class Zombie {
 
-    private LatLng position;
-
-    public void setPosition(LatLng position) {
-        this.position = position;
-    }
-
-    public LatLng getPosition() {
-        return position;
-    }
 }
